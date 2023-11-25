9604b244457096bf24a45fd5d7b34883b7486851
Yufeng Zhao <115388472+epsilondylan@users.noreply.github.com>
2023-11-25 10:51:39 +0800

    Add files via upload

--- image_service_grpc.pb.go
+++ image_service_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ImageServiceClient interface {
 	GetSingleImg(ctx context.Context, in *ResizeRequest, opts ...grpc.CallOption) (*GetSingleImgResult, error)
+	GetMultiImgs(ctx context.Context, in *Imgrequests, opts ...grpc.CallOption) (ImageService_GetMultiImgsClient, error)
 }
 
 type imageServiceClient struct {
@@ -42,11 +43,44 @@ func (c *imageServiceClient) GetSingleImg(ctx context.Context, in *ResizeRequest
 	return out, nil
 }
 
+func (c *imageServiceClient) GetMultiImgs(ctx context.Context, in *Imgrequests, opts ...grpc.CallOption) (ImageService_GetMultiImgsClient, error) {
+	stream, err := c.cc.NewStream(ctx, &ImageService_ServiceDesc.Streams[0], "/image_service.ImageService/getMultiImgs", opts...)
+	if err != nil {
+		return nil, err
+	}
+	x := &imageServiceGetMultiImgsClient{stream}
+	if err := x.ClientStream.SendMsg(in); err != nil {
+		return nil, err
+	}
+	if err := x.ClientStream.CloseSend(); err != nil {
+		return nil, err
+	}
+	return x, nil
+}
+
+type ImageService_GetMultiImgsClient interface {
+	Recv() (*Imgresult, error)
+	grpc.ClientStream
+}
+
+type imageServiceGetMultiImgsClient struct {
+	grpc.ClientStream
+}
+
+func (x *imageServiceGetMultiImgsClient) Recv() (*Imgresult, error) {
+	m := new(Imgresult)
+	if err := x.ClientStream.RecvMsg(m); err != nil {
+		return nil, err
+	}
+	return m, nil
+}
+
 // ImageServiceServer is the server API for ImageService service.
 // All implementations must embed UnimplementedImageServiceServer
 // for forward compatibility
 type ImageServiceServer interface {
 	GetSingleImg(context.Context, *ResizeRequest) (*GetSingleImgResult, error)
+	GetMultiImgs(*Imgrequests, ImageService_GetMultiImgsServer) error
 	mustEmbedUnimplementedImageServiceServer()
 }
 
@@ -56,6 +90,9 @@ type UnimplementedImageServiceServer struct {
 
 func (UnimplementedImageServiceServer) GetSingleImg(context.Context, *ResizeRequest) (*GetSingleImgResult, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method GetSingleImg not implemented")
+}
+func (UnimplementedImageServiceServer) GetMultiImgs(*Imgrequests, ImageService_GetMultiImgsServer) error {
+	return status.Errorf(codes.Unimplemented, "method GetMultiImgs not implemented")
 }
 func (UnimplementedImageServiceServer) mustEmbedUnimplementedImageServiceServer() {}
 
@@ -88,6 +125,27 @@ func _ImageService_GetSingleImg_Handler(srv interface{}, ctx context.Context, de
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ImageService_GetMultiImgs_Handler(srv interface{}, stream grpc.ServerStream) error {
+	m := new(Imgrequests)
+	if err := stream.RecvMsg(m); err != nil {
+		return err
+	}
+	return srv.(ImageServiceServer).GetMultiImgs(m, &imageServiceGetMultiImgsServer{stream})
+}
+
+type ImageService_GetMultiImgsServer interface {
+	Send(*Imgresult) error
+	grpc.ServerStream
+}
+
+type imageServiceGetMultiImgsServer struct {
+	grpc.ServerStream
+}
+
+func (x *imageServiceGetMultiImgsServer) Send(m *Imgresult) error {
+	return x.ServerStream.SendMsg(m)
+}
+
 // ImageService_ServiceDesc is the grpc.ServiceDesc for ImageService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -100,6 +158,12 @@ var ImageService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _ImageService_GetSingleImg_Handler,
 		},
 	},
-	Streams:  []grpc.StreamDesc{},
+	Streams: []grpc.StreamDesc{
+		{
+			StreamName:    "getMultiImgs",
+			Handler:       _ImageService_GetMultiImgs_Handler,
+			ServerStreams: true,
+		},
+	},
 	Metadata: "image_service.proto",
 }
